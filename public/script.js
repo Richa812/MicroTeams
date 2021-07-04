d70ace12fbca22c4f7011f7d64c57519183e6167
Richa812 <58879930+Richa812@users.noreply.github.com>
2021-07-04 15:56:32 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -6,12 +6,12 @@ myVideo.muted=true;
 var peer = new Peer(undefined,{
     path:'/peerjs',
     host:'/',
-    port:'443'
+    port:'3030'
 
 });
 
 alert('Welcome to Micro-teams!');
-let  username = prompt("Enter username");
+const  username = prompt("Please enter your name");
 
 let myVideoStream
 const peers ={};
@@ -27,68 +27,98 @@ navigator.mediaDevices.getUserMedia({
         const video = document.createElement('video')
         call.on('stream',userVideoStream =>{
             addVideoStream(video,userVideoStream)
-        })
-    })
+        });
+    });
    socket.on('user-connected',(userId)=>{
     connectToNewUser(userId,stream);
-})
-
-let text = $('input')
-
-
-$('html').keydown((e)=>{
-    if(e.which == 13 && text.val().length !=0){
-        socket.emit('message',text.val());
-        text.val('')
-    }
+});
 });
 
-socket.on('createMessage',message => {
-   $('.messages').append(`<li class="message"><b>${username}</b><br/>${message}</li>`) ;
-   scrollToBottom()
-})
-})
-
-socket.on('user-disconnected',userId =>{
-  if(peers[userId]) peers[userId].close()
-})
-
 peer.on('open',id=>{
-    socket.emit('join-room',ROOM_ID,id);
- 
+  socket.emit('join-room',ROOM_ID,id,username);
+
 })
 
 
 
 const connectToNewUser= (userId,stream)=> {
-   const  call =peer.call(userId,stream)
-   const video=document.createElement('video')
-   call.on('stream',userVideoStream => {
-      addVideoStream(video,userVideoStream)
-   })
+ const  call =peer.call(userId,stream)
+ const video=document.createElement('video')
+ call.on('stream',userVideoStream => {
+    addVideoStream(video,userVideoStream)
+ })
 
-   call.on('close',()=>{
-     video.remove()
-   })
+ call.on('close',()=>{
+   video.remove()
+ })
 
-   peers[userId]=call
+ peers[userId]=call
 }
 
 
 
 const addVideoStream = (video,stream)=>{
-    video.srcObject= stream;
-    video.addEventListener('loadedmetadata',()=>{
-        video.play();
-    })
-    videoGrid.append(video);
+  video.srcObject= stream;
+  video.addEventListener('loadedmetadata',()=>{
+      video.play();
+  })
+  videoGrid.append(video);
 }
+
+socket.on('user-disconnected',userId =>{
+  if(peers[userId]) peers[userId].close()
+})
+
+
+//chat-box
+
+let text = document.querySelector("#chat_message");
+let send = document.getElementById("send");
+let messages = document.querySelector(".messages");
+
+send.addEventListener("click", (e) => {
+  if (text.value.length !== 0) {
+    socket.emit("message", text.value);
+    text.value = "";
+  }
+});
+
+text.addEventListener("keydown", (e) => {
+  if (e.key === "Enter" && text.value.length !== 0) {
+    socket.emit("message", text.value);
+    text.value = "";
+  }
+});
+
+socket.on("createMessage", (message, userName) => {
+  messages.innerHTML =
+    messages.innerHTML +
+    `<div class="message">
+        <b> <span> ${
+          userName
+        }</span> </b>
+        <span>${message}</span>
+    </div>`;
+});
+
+
+
 
 const scrollToBottom=() =>{
     let d=$('.main__chat__window');
     d.scrollTop(d.prop("scrollHeigth"));
 }
 
+
+//invite people
+const inviteButton = document.querySelector("#Invite");
+
+inviteButton.addEventListener("click", (e) => {
+  prompt(
+    "Copy and send this to add people",
+    window.location.href
+  );
+});
 const muteUnmute = () => {
     const enabled = myVideoStream.getAudioTracks()[0].enabled;
     if(enabled){
@@ -144,3 +174,5 @@ const setUnmuteButton = () => {
     `
     document.querySelector('.main__video_button').innerHTML = html;
   }
+
+  //https://sleepy-springs-59737.herokuapp.com/
